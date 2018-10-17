4c49b4ac52ad8e166789360dbf87bfc9d76b2e26
IvannKim <41999496+IvannKim@users.noreply.github.com>
2018-10-17 17:11:06 +0900

    Add files via upload

---
+++ FtoC.js
@@ -0,0 +1,4 @@
+const userNumber = Number(prompt('섭씨로 변경할 화씨단위로 온도를 입력하시오'))
+const fToC = num => (num - 32) / 1.8
+const cResult = fToC(userNumber)
+console.log(`화씨온도 : ${userNumber} => 섭씨온도 : ${cResult}`)
