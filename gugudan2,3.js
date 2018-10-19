e532a0483cdfb2d7f0b79a7daa19ea28738468b1
IvannKim <41999496+IvannKim@users.noreply.github.com>
2018-10-19 15:25:22 +0900

    Add files via upload

---
+++ gugudan2,3.js
@@ -0,0 +1,10 @@
+//2,3단 구현하기
+
+const getGugudanArray = (num, num2) => {
+    for (i = 1; i < 10; i++) {
+        console.log(`${num} x ${i} = ${num * i}`)
+        console.log(`${num2} x ${i} = ${num2 * i}`)
+    }
+}
+
+let twoAndThreeDan = getGugudanArray(2, 3)
