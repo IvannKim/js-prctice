4b0971645e8a186eac06e5ff1e0dd0d07d614fda
IvannKim <41999496+IvannKim@users.noreply.github.com>
2018-10-19 15:25:05 +0900

    Add files via upload

--- evenSum.js
+++ evenSum.js
@@ -1,1 +1,12 @@
 //1에서 100까지 짝수의 합을 구해 봅시다.
+function getArrayNum() {
+    let array = [];
+    for (i = 1; i <= 100; i++) if (i % 2 == 0) array.push(i)
+    return array
+}
+
+const array = getArrayNum()
+
+let arraySum = array.reduce((arr, crr) => arr + crr);
+
+console.log(arraySum)
